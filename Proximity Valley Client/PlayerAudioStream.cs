f99c330b4f51e1ecfb160d3f8ab0814497acd854
Suhrii <suhriyt@gmail.com>
2025-07-27 22:05:04 +0200

    for audio panning

---
+++ Proximity Valley Client/PlayerAudioStream.cs
@@ -0,0 +1,48 @@
+﻿using NAudio.Wave;
+using NAudio.Wave.SampleProviders;
+
+
+namespace Proximity_Valley
+{
+    public class PlayerAudioStream
+    {
+        public BufferedWaveProvider Buffer { get; }
+        public VolumeSampleProvider VolumeProvider { get; }
+        public PanningSampleProvider PanningProvider { get; }
+        public WaveOutEvent Output { get; }
+
+        public PlayerAudioStream(WaveFormat format, int bufferSeconds, int device, float initialVolume, float initialPan)
+        {
+            Buffer = new BufferedWaveProvider(format)
+            {
+                DiscardOnBufferOverflow = true,
+                BufferLength = format.AverageBytesPerSecond * bufferSeconds
+            };
+
+            var sample = Buffer.ToSampleProvider();
+            PanningProvider = new PanningSampleProvider(sample) { Pan = initialPan };
+            VolumeProvider = new VolumeSampleProvider(PanningProvider) { Volume = initialVolume };
+
+            Output = new WaveOutEvent { DeviceNumber = device };
+            Output.Init(VolumeProvider);
+            Output.Play();
+        }
+
+        public void AddSamples(byte[] data, int offset, int count)
+        {
+            Buffer.AddSamples(data, offset, count);
+        }
+
+        public void UpdatePanAndVolume(float pan, float volume)
+        {
+            PanningProvider.Pan = pan;
+            VolumeProvider.Volume = volume;
+        }
+
+        public void Dispose()
+        {
+            Output?.Dispose();
+        }
+    }
+
+}
