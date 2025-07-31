56975d7b80de4cc1568c019c1b5255197256a823
Salmi-168 <juliansalm@gmx.de>
2025-07-31 10:53:43 +0200

    moved ServerConfig to separate file

--- Proximity Valley Server/VoiceServer.cs
+++ Proximity Valley Server/VoiceServer.cs
@@ -8,14 +8,7 @@ using Newtonsoft.Json;
 
 namespace Proximity_Valley_Server;
 
-public class ServerConfig
-{
-    public int ListenPort { get; set; } = 5000;
-    public string EncryptionKey { get; set; } = "Gmj%8k%65xmpCjvzLG8FkUYz3FPyfTnv";
-    public string EncryptionIV { get; set; } = "LmiEy!AF3bTT$Pwk";
-}
-
-partial class VoiceServer
+public class VoiceServer
 {
     private readonly UdpClient udpServer;
     private readonly Dictionary<IPEndPoint, string> clientMap = [];
